ee1fed14d8a6991a48c0f03ad2ebe0ff070f8139
T-Chabalala <thoko.joy16@gmail.com>
2025-05-31 22:09:50 +0200

    Added  5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,5 +4,5 @@ const num = parseInt(arg);
 if (isNaN(num)) {
     console.log('Not a number');
 } else {
-    console.log(`my number: ${parseInt(num)}`);
+    console.log(`The number is ${num}`);
 }
